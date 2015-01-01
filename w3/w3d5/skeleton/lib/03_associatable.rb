1448e690a419346c2c0e19507585d8ea097d8082
Luke <luke8520@gmail.com>
2014-12-31 18:01:04 -0800

    changed to underscore incase CamelCase

--- w3/w3d5/skeleton/lib/03_associatable.rb
+++ w3/w3d5/skeleton/lib/03_associatable.rb
@@ -28,7 +28,7 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key  = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    @foreign_key  = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
     @class_name   = options[:class_name]  || name.to_s.capitalize.singularize
     @primary_key  = options[:primary_key] || :id
   end
