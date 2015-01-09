924fed5ba35d8e6317095c021c23d12dba6c5569
Luke <luke8520@gmail.com>
2015-01-08 19:33:45 -0800

    modified get to collection in routes for users activation

--- w4/w4d4/music_app/app/mailers/user_mailer.rb
+++ w4/w4d4/music_app/app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "Albus Dumbledore <a.dumbledore@hogwarts.edu>"
   def activation_email(user)
     @user = user
-    @activation_link = users_activate_url(activation_token: @user.activation_token, email: @user.email)
+    @activation_link = activate_users_url(activation_token: @user.activation_token, email: @user.email)
     mail(to: @user.email, subject: "Congratulations!")
   end
 end
--- w4/w4d4/music_app/config/routes.rb
+++ w4/w4d4/music_app/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "bands#index"
 
-  get "users/activate", to: "users#activate"
   resources :notes, only: [:create, :destroy]
   resources :tracks, except: [:new, :index]
   resources :albums, except: [:new, :index] do
@@ -10,6 +9,8 @@ Rails.application.routes.draw do
   resources :bands do
     resources :albums, only: :new
   end
-  resources :users
+  resources :users do
+    get "activate", on: :collection
+  end
   resource :session
 end
